33a929e590075d3207e691ec724ca399f7ff2002
Timothy Yu <tmichael.yu@gmail.com>
2021-11-02 16:04:49 -0400

    sql tables update with feedback from trainer

--- sql_commands.sql
+++ sql_commands.sql
@@ -1,40 +1,37 @@
+select  * from clients;
+select * from userdevices;
+select * from wishlist;
+select * from games;
+
+drop table IF EXISTS clients cascade;
+drop table IF EXISTS users cascade;
+drop table IF EXISTS wishlist cascade;
+drop table IF EXISTS userdevices cascade;
+drop table IF EXISTS games cascade; 
+
 create table clients (
     clientID serial,
     clientname varchar(255) not null,
     clientemail varchar(255) not null,
+    username varchar(255) not null,
     primary key (clientID)
 );
 
-create table users (
-    wishlistID serial,
-    uclientID int not null,
-    username varchar(255) not null,
-    primary key (wishlistID)
-    foreign key (uclientID) references clients(clientID)
-);
-
-create table userdevices (
-    entryID serial not null,
-    dclientID serial,
-    Switch boolean,
-    XboxOne boolean,
-    XboxSeriesX boolean,
-    PS5 boolean,
-    PS4 boolean,
-    PC boolean,
-    OS varchar(255),
-    ram varchar(255),
-    cpu varchar(255),
-    gpu varchar(255),
-    primary key (entryID),
-    foreign key (dclientID) references clients(clientID)
-    
-);
+/*
+insert into clients (clientname,clientemail,username)
+values ('Timothy','testemail@gmail.com','timothyyu');
+insert into clients (clientname,clientemail,username)
+values ('Kenneth','testemail2@gmail.com','kbradburn');
+select * from clients;
+*/
 
 create table wishlist (
-    gamename varchar(255) not null,
-    wishlistID int not null,
-    foreign key (wishlistID) references users(wishlistID)
+    wishlistID serial not null,
+    wishclientID int not null,
+    wishgameID int not null,
+    foreign key (wishclientID) references clients(clientID),
+    foreign key (wishgameID) references games(gamelineID),
+    primary key (wishlistID)
 );
 
 create table games (
@@ -43,16 +40,44 @@ create table games (
     gamegenre varchar(255) not null,
     gameprice DECIMAL(13, 2) not null,
     bestvendor varchar(255) not null,
+    link varchar(255) not null,
     primary key (gamelineID)
 );
 
-create table vendors (
-    vendorlineID serial,
-    connectionkey int not null,
-    link varchar(255) not null,
-    primary key (vendorlineID),
-    foreign key (connectionkey) references games(gamelineID)
+drop table device cascade; 
+
+create table Device (
+    DevLineID serial,
+    DevclientID int not null,
+    DeviceName varchar(50) not null,
+    PCref int unique,
+    primary key (DevLineID),
+    foreign key (DevclientID) references clients(clientID)
 );
+/*
+insert into device (DevclientID,DeviceName)
+values (1,'PS4');
 
+select * from Device;
 
+insert into device (DevclientID,DeviceName,PCref)
+values (2,'PC', 1);
 
+insert into PCowner (ownclientID,PCref,OS,RAM,CPU,GPU)
+values (2,1,'Windows 10','16GB','AMD Ryzen 5 3600X','NVIDIA 2060S');
+
+select * from PCowner;
+*/
+
+create table PCowner (
+    LineID serial not null,
+    ownclientID int not null,
+    PCref int not null,
+    OS varchar(255) not null,
+    RAM varchar(255) not null,
+    CPU varchar(255) not null,
+    GPU varchar(255) not null,
+    foreign key (ownclientID) references clients(clientID),
+    foreign key (PCref) references Device(PCref),
+    primary key (LineID)
+);
